0c1df06e14ba775e8fa33c73f5878a252542d913
John Melton <jtmelton@gmail.com>
2020-11-14 00:32:44 -0500

    minor changes and rev again

---
+++ src/main/kotlin/com/github/jtmelton/semgrepideaplugin/actions/MyTestAction.kt
@@ -0,0 +1,22 @@
+package com.github.jtmelton.semgrepideaplugin.actions
+
+import com.github.jtmelton.semgrepideaplugin.services.MyProjectService
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.Logger
+
+
+class MyTestAction : AnAction() {
+    override fun update(e: AnActionEvent) {
+        // Using the event, evaluate the context, and enable or disable the action.
+        println("inside the MyTestAction action 1")
+        Logger.getInstance("MyTestAction").error("action test MyTestAction 1")
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        // Using the event, implement an action. For example, create and show a dialog.
+        println("inside the MyTestAction action 2")
+        Logger.getInstance("MyTestAction").error("action test MyTestAction 2")
+    }
+}
